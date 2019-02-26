c9ba532a0a67e250a551bf17383ed64793c20661
FlowingFog <46822877+FlowingFog@users.noreply.github.com>
2019-02-25 20:56:49 -0800

    Add files via upload

---
+++ Arduino_MultiButton_Read_off_One_Port.ino
@@ -0,0 +1,23 @@
+double buttonRead;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+// Serial.println(analogRead(A0));
+ buttonRead = analogRead(A0);
+ if (buttonRead >= 185 && buttonRead <= 215) {
+  Serial.println("Button 4 Pressed");
+ }
+ if (buttonRead >= 235 && buttonRead <= 265) {
+  Serial.println("Button 3 Pressed");
+ }
+   if (buttonRead >= 315 && buttonRead <= 345) {
+  Serial.println("Button 2 Pressed");
+ }
+ if (buttonRead >= 485 && buttonRead <= 515) {
+  Serial.println("Button 1 Pressed");
+ }
+}
